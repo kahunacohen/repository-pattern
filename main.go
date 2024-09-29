b5ef02d5ce6b354a5b827de7c9df94be476c31a9
Aharon Cohen <aharonc@matav.org.il>
2024-09-29 17:30:13 +0300

    added router

--- main.go
+++ main.go
@@ -1,8 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/kahunacohen/repo-pattern/controllers"
+)
 
 func main() {
 
-	fmt.Println("main")
+	http.HandleFunc("/users/export", controllers.ExportUsers)
+
+	// Start the server on port 8080
+	fmt.Println("Server is listening on port 8080...")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		fmt.Println("Error starting server:", err)
+	}
 }
