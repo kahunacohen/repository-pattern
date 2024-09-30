13fd7139c9c690d3827177ddd9abaf538e3fb3c1
Aharon Cohen <aharonc@matav.org.il>
2024-09-30 09:22:18 +0300

    add 202

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -1,10 +1,16 @@
 package controllers
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
 )
 
 func ExportUsers(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hi")
+	w.WriteHeader(http.StatusAccepted)
+	response := map[string]string{
+		"message": "Export process has been initiated.",
+		"jobId":   "12345",
+	}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(response)
 }
